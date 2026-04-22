81702ff538b1dd90b9030ca92106be8575d54e78
Novaxll <zS24003986@estudiantes.uv.mx>
2026-04-22 09:52:26 -0600

    Auto-generated commit

---
+++ .angular/cache/21.1.5/cv-musk-style/vite/deps/@angular_fire_compat.js
@@ -0,0 +1,24 @@
+import {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+} from "./chunk-RWFPC56O.js";
+import "./chunk-3V5KFESH.js";
+import "./chunk-BOSH4NMU.js";
+import "./chunk-7LJXV3FM.js";
+import "./chunk-VC3KBLVO.js";
+export {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+};
